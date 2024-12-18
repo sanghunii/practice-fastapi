5fb90dd58aaa330bd92faa991f5e0c3fcf451b20
박상훈 <sanghoon20020403@gmail.com>
2024-12-18 10:25:13 +0900

    .

--- api/post.py
+++ api/post.py
@@ -10,3 +10,8 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/{item_id}")
 def read_item(item_id: str, skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+@app.post("/")
+def home_post(msg: str):
+    return {"Hello": "POST", "msg": msg}
