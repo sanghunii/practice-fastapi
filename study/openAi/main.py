9c9fb76d8b2f2f9b6590658ca084eb07f99b18bd
박상훈 <sanghoon20020403@gmail.com>
2025-03-28 12:06:58 +0900

    streaming response 방식 테스트 / 아직 미완

--- study/openAi/main.py
+++ study/openAi/main.py
@@ -12,6 +12,7 @@ load_dotenv()
 API_KEY = os.environ.get('API_KEY')
 
 origins = [
+    'http://localhost:3000',
     'http://localhost:3001'
 ]
 
@@ -31,23 +32,48 @@ async def chat():
     )    
     
     ##Create Chat
-    response = client.chat.completions.create(
+    stream = client.chat.completions.create(
     model="gpt-4o",
     messages=[
         {"role": "system", "content": "나의 프롬프트 엔지니어링 코치가 되어줘."},
         {"role": "user", "content": "어떻게 하면 프롬프트 엔지니어링을 잘 할수 있을까?"}
-    ]
+    ],
+    stream=True
     )
     ##parsing(get chatbot's answer)
-    answer: str = response.choices[0].message.content
     
+    # streaming response
+    for chunck in stream:
+        if chunck.choices[0].delta.content is not None:
+            return {'anser' : chunck.choices[0].delta.content}
     
-    return {'answer': answer}
 
 
-""" 코드 설명
-answer: str = response.choices[0].message.content.encode(encoding="utf-8").decode()
+"""
+client = OpenAI()
 
-**reference**
+stream = client.chat.completions.create(
+    model="gpt-4o",
+    messages=[
+        {
+            "role": "user",
+            "content": "Say 'double bubble bath' ten times fast.",
+        },
+    ],
+    stream=True,
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
+"""
+
+
+""" referecne
+
 https://chatgpt.com/share/6778fd12-d9cc-800b-ac1f-eeb0bd0b04f5
+
+
+""previous response code
+answer: str = response.choices[0].message.content
 """
